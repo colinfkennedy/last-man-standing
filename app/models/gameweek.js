d5c9ac049ed1daa43f256d11e14e37b91e3878a2
Colin Kennedy <colin.kennedy@intercom.io>
2021-06-28 21:56:59 +0100

    Move alphabet picks to gameweek model

--- app/models/gameweek.js
+++ app/models/gameweek.js
@@ -1,17 +1,20 @@
 import Model, { attr, hasMany } from '@ember-data/model';
 import { isPresent } from '@ember/utils';
+import { inject as service } from '@ember/service';
+import EmberObject from '@ember/object';
 
 export default class GameweekModel extends Model {
   @attr('string') label;
   @hasMany('fixture') fixtures;
   @hasMany('selection') selections;
+  @service store;
 
   get losingTeams() {
     return this.fixtures.map((fixture) => fixture.losingTeam).compact();
   }
 
   get losingSelections() {
-    return this.selections.filter((selection) => {
+    return this.selectionsWithDefaults.filter((selection) => {
       if (isPresent(selection.club)) {
         return isPresent(
           this.losingTeams.findBy('name', selection.get('club.name'))
@@ -20,6 +23,42 @@ export default class GameweekModel extends Model {
         return false;
       }
     });
+  }
+
+  get selectionsWithDefaults() {
+    let babbers = this.store.peekAll('babber');
+
+    return babbers.map((babber) => {
+      let gameweekSelection = this.selections.findBy(
+        'babber.name',
+        babber.name
+      );
+      return gameweekSelection || this.defaultSelection(babber);
+    });
+  }
+
+  defaultSelection(babber) {
+    let clubs = this.store.peekAll('club');
+    let alreadySelected = babber.selections.map((selection) =>
+      selection.get('club.name')
+    );
+    //TODO Fix this for games that don't start on gameweek 1.
+    let previousAlphabetPicks =
+      parseInt(this.label) - 1 - alreadySelected.length;
+
+    let club = clubs
+      .filter((club) => !alreadySelected.includes(club.name))
+      .sortBy('name')
+      .slice(previousAlphabetPicks, 100).firstObject;
+
+    let alphabetSelection = EmberObject.create({
+      babber,
+      club,
+      gameweek: this,
+      isAlphabetPick: true,
+    });
+
+    return alphabetSelection;
   }
 
   get start() {
--- app/models/selection.js
+++ app/models/selection.js
@@ -1,7 +1,8 @@
-import Model, { belongsTo } from '@ember-data/model';
+import Model, { attr, belongsTo } from '@ember-data/model';
 
 export default class SelectionModel extends Model {
   @belongsTo('gameweek') gameweek;
   @belongsTo('babber') babber;
   @belongsTo('club') club;
+  @attr('boolean', { defaultValue: false }) isAlphabetPick;
 }
