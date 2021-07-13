7de7e3400437cee65a130183df16675a918d254c
Colin Kennedy <colin.kennedy@intercom.io>
2021-07-13 18:30:53 +0100

    Add totals for games won and lost

--- app/components/leaderboard/leaderboard.js
+++ app/components/leaderboard/leaderboard.js
@@ -1,20 +1,43 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
+import { cached } from '@glimmer/tracking';
 
 export default class LeaderboardLeaderboardComponent extends Component {
   @service store;
 
-  get orderedTotals() {
-    return this.store.peekAll('babber').map((babber) => {
-      let babberStanding = {
-        played: 0,
-        won: 0,
-        drawn: 0,
-        lost: 0,
-        total: 0,
-        babber: babber,
-      };
-      return babberStanding;
+  @cached
+  get gamesPlayed() {
+    return this.args.games.length;
+  }
+
+  @cached
+  get gamesWithWinners() {
+    return this.args.games.reject((game) => {
+      return game.winner.get('id') === undefined;
     });
+  }
+
+  get orderedTotals() {
+    return this.args.babbers
+      .map((babber) => {
+        let played = this.gamesPlayed;
+        let won = this.args.games.filterBy('winner.id', babber.id).length;
+        let lost = this.gamesWithWinners.rejectBy(
+          'winner.id',
+          babber.id
+        ).length;
+        let total = won * 40 - played * 5;
+        let babberStanding = {
+          played,
+          won,
+          drawn: 0,
+          lost,
+          total: `€${total}`,
+          babber: babber,
+        };
+        return babberStanding;
+      })
+      .sortBy('total')
+      .reverse();
   }
 }
--- app/routes/leaderboard.js
+++ app/routes/leaderboard.js
@@ -8,6 +8,7 @@ export default class LeaderboardRoute extends Route {
   model() {
     return RSVP.hash({
       babbers: this.store.findAll('babber'),
+      games: this.store.findAll('game'),
     });
   }
 }
