7d7e53302a73a4812e223f834d0a295b98fc4c9d
Colin Kennedy <colin.kennedy@intercom.io>
2021-07-18 12:23:08 +0100

    Add model adapters to add ordering query param

---
+++ app/adapters/club.js
@@ -0,0 +1,9 @@
+import ApplicationAdapter from './application';
+
+export default class ClubAdapter extends ApplicationAdapter {
+  // eslint-disable-next-line no-unused-vars
+  urlForFindAll(modelName, snapshot) {
+    let baseUrl = this.buildURL(modelName);
+    return `${baseUrl}?order=name`;
+  }
+}
