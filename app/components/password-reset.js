475b81ff09d2495caa6807c792b1b306e3e594c1
Colin Kennedy <colin.kennedy@intercom.io>
2022-03-22 16:30:02 +0000

    Add green confirmation message to password reset

--- app/components/password-reset.js
+++ app/components/password-reset.js
@@ -4,17 +4,18 @@ import { action } from '@ember/object';
 
 export default class PasswordResetComponent extends Component {
   @tracked email;
-  @tracked message;
+  @tracked errorMessage;
+  @tracked confirmationMessage;
 
   @action
   resetPassword() {
     console.log(`Resetting for ${this.email}`);
     Parse.User.requestPasswordReset(this.email)
       .then(() => {
-        this.message = 'Sent reset email';
+        this.confirmationMessage = 'Sent reset email';
       })
       .catch((error) => {
-        this.message = error.message;
+        this.errorMessage = error.message;
         console.error(`Error: ${error.code} ${error.message}`);
       });
   }
